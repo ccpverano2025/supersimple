fe087acd04e5b37fe16493a3c4477a217a89c5e1
ccpverano2025 <ccpverano2025@gmail.com>
2025-08-22 11:18:07 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,2 +1,2 @@
 // URL de tu Google Apps Script Web App (ya probada)
-const SHEETS_ENDPOINT = "https://script.google.com/macros/s/AKfycbyd4xZeCgmnXixSi6J5KplqOmPC5-gBveeK9R6HXwn6TVKPp2Ql6qokn0_wiM1hmxhy/exec";
+const SHEETS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxKbsJW5xsehpGShiIhs8NU9qL34G8RuwBe7H0jVTBUwSR7TpSVDxPhxc348o2FLezZ/exec";
